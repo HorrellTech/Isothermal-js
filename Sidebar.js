4d1dbba7978ce48120b5038ccdb2b9188611722c
SynKrown Games <horrelltechnologies@gmail.com>
2024-03-30 21:56:47 +1300

    update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -1,11 +1,13 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("file-list-container").style.display = "block"; 
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("file-list-container").style.display = "none"; 
 }
 
 // Add swipe event listeners
