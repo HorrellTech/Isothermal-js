9f796aac3b6dd3a3e1d21e14ddd2c7a2ca918a9f
SynKrown Games <horrelltechnologies@gmail.com>
2024-03-30 22:35:29 +1300

    update Js/game.js

--- Js/game.js
+++ Js/game.js
@@ -2095,28 +2095,6 @@ function screen_get_height()
     return (screen.height);
 }
 
-// Return a number that is always positive
-function keep_positive(x)
-{
-    if(x < 0)
-    {
-        x *= -1;
-    }
-
-    return (x);
-}
-
-// Return a number that is always negative
-function keep_negative(x)
-{
-    if(x > 0)
-    {
-        x *= -1;
-    }
-
-    return (x);
-}
-
 // Returns whether or not a rectangle is inside the view bounds
 function within_view(x1, y1, x2, y2)
 {
@@ -2129,85 +2107,6 @@ function within_view(x1, y1, x2, y2)
         x1 + (x2) >= xx && y1 + (y2) >= yy && x1 <= w && y1 <= h
     );
 }
-
-// Create a new vector2
-function vec2(x, y)
-{
-    this.x = x;
-    this.y = y;
-}
-
-// Create a new vector2
-function vec3(x, y, z)
-{
-    this.x = x;
-    this.y = y;
-    this.z = z;
-}
-
-/**
- * Helper function to determine whether there is an intersection between the two polygons described
- * by the lists of vertices. Uses the Separating Axis Theorem
- *
- * @param a an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
- * @param b an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
- * @return true if there is any intersection between the 2 polygons, false otherwise
- */
-function doPolygonsIntersect (a, b) {
-    var polygons = [a, b];
-    var minA, maxA, projected, i, i1, j, minB, maxB;
-
-    for (i = 0; i < polygons.length; i++) {
-
-        // for each polygon, look at each edge of the polygon, and determine if it separates
-        // the two shapes
-        var polygon = polygons[i];
-        for (i1 = 0; i1 < polygon.length; i1++) {
-
-            // grab 2 vertices to create an edge
-            var i2 = (i1 + 1) % polygon.length;
-            var p1 = polygon[i1];
-            var p2 = polygon[i2];
-
-            // find the line perpendicular to this edge
-            var normal = { x: p2.y - p1.y, y: p1.x - p2.x };
-
-            minA = maxA = undefined;
-            // for each vertex in the first shape, project it onto the line perpendicular to the edge
-            // and keep track of the min and max of these values
-            for (j = 0; j < a.length; j++) {
-                projected = normal.x * a[j].x + normal.y * a[j].y;
-                if (isUndefined(minA) || projected < minA) {
-                    minA = projected;
-                }
-                if (isUndefined(maxA) || projected > maxA) {
-                    maxA = projected;
-                }
-            }
-
-            // for each vertex in the second shape, project it onto the line perpendicular to the edge
-            // and keep track of the min and max of these values
-            minB = maxB = undefined;
-            for (j = 0; j < b.length; j++) {
-                projected = normal.x * b[j].x + normal.y * b[j].y;
-                if (isUndefined(minB) || projected < minB) {
-                    minB = projected;
-                }
-                if (isUndefined(maxB) || projected > maxB) {
-                    maxB = projected;
-                }
-            }
-
-            // if there is no overlap between the projects, the edge we are looking at separates the two
-            // polygons, and we know there is no overlap
-            if (maxA < minB || maxB < minA) {
-                console.log("polygons don't intersect!");
-                return false;
-            }
-        }
-    }
-    return true;
-};
 
 //#newfile Fake3D
 
