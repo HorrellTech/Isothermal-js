f92dac55119c9c1a760d9b82add634db4a93c424
SynKrown Games <horrelltechnologies@gmail.com>
2024-03-31 16:09:16 +1300

    update playground.js

--- playground.js
+++ playground.js
@@ -26,7 +26,7 @@ window.addEventListener('touchstart', function(event) {
 
 window.addEventListener('touchmove', function(event) {
   let touchX = event.touches[0].clientX;
-  if(startTouchX > 32)
+  if(touchx.pagex > 32)
   {
     if (touchX - startTouchX > 50) {
       sidePanel.style.left = '0';
