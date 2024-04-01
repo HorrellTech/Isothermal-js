c70427b0efdb4d7d2303fbf25ccebd0513bde3d4
SynKrown Games <horrelltechnologies@gmail.com>
2024-04-01 14:26:27 +1300

    update Js/audio.js

--- Js/audio.js
+++ Js/audio.js
@@ -1,3 +1,7 @@
-const synth = new Tone.Synth().toDestination();
 
-synth.triggerAttackRelease("C4", "8n");
+function audio_test()
+{
+  const synth = new Tone.Synth().toDestination();
+  
+  synth.triggerAttackRelease("C4", "8n");
+}
