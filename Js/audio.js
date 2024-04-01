dc241b0184504076780eefb8aea5627c2f6c363c
SynKrown Games <horrelltechnologies@gmail.com>
2024-04-01 14:40:40 +1300

    update Js/audio.js

--- Js/audio.js
+++ Js/audio.js
@@ -1,5 +1,7 @@
 import * as Tone from 'tone';
 
+let _channel = [];
+let _synth = [];
 
 // Test here to make sure it works
 function audio_test()
@@ -7,4 +9,29 @@ function audio_test()
   const synth = new Tone.Synth().toDestination();
   
   synth.triggerAttackRelease("C4", "8n");
+}
+
+function audio_init()
+{
+  
+}
+
+function audio_add_channel()
+{
+  
+}
+
+function audio_add_synth()
+{
+  
+}
+
+function audio_play()
+{
+  
+}
+
+function audio_play_note()
+{
+  
 }
