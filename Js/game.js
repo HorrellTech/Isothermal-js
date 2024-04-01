09969d5ea8944a4603de6f27268d674375deee5e
SynKrown Games <horrelltechnologies@gmail.com>
2024-04-01 16:48:26 +1300

    update Js/game.js

--- Js/game.js
+++ Js/game.js
@@ -1,6 +1,7 @@
 /*
   This is the main game file for the framework. It handles objects and advanced math functions.
 */
+import * as Tone from 'tone';
 
 // CONSTANTS
 const canvasId = 'canvasArea';
